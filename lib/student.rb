8778dd71c9c59b95112acb894e836e1c8877bd42
Steven Senkowski <scsenkowski@gmail.com>
2017-12-26 20:24:58 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,10 +35,9 @@ attr_reader :id
 
     DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-
   end
 
   def self.create(name:, grade:)
-    student.save = Student.new(name, grade)
+    student = Student.new(name, grade)
   end
 end
